96027afc7343ba4136a0298b4468d6b31741dba5
CHIZY'S-TECH <felixenesko@gmail.com>
2025-09-03 15:50:34 +0100

    Create A book.py
    AccountBook: 
     is a simple Python program that displays stored usernames and passwords when you call the profile/account name.  
    It’s like a mini password manager built with Python.
    
    
    
    ## ✨ Features
    - Store multiple accounts with username & password
    - Display credentials by calling the account name
    - Easy to extend and customize

---
+++ A book.py
@@ -0,0 +1,20 @@
+`class Accounts:
+	def __init__(self, phone_email, username, password):
+		self.phone_email = phone_email
+		self.username = username
+		self.password = password
+	
+	def show(self):
+		print(f"{self.phone_email} : {self.username} : {self.password}")
+		
+facebook = Accounts("07148254493@gmail",  "Terer Somtdili",  "656956")
+
+google = Accounts("felixenesko", "felixenesko", "Eneojo1@9862@Justification")
+
+istagram = Accounts ("82288458888", "Zanaskbd", "lgdgadon")
+
+google3 = Accounts("123598898", "paisefsko", "stifichhkffation")
+
+yahoo = Accounts("125888788", "fjkfjvhesko", "967284vn rosoft")
+
+yahoo.show() `
